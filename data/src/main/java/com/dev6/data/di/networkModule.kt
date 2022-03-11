8b134078f23eca3205d71a86f8096329dff6a411
vov1252@naver.com <vov1252@naver.com>
2022-03-11 15:31:38 +0900

    [UPDATE]  API 및 Retrofit 모듈 추가

--- data/src/main/java/com/dev6/data/di/networkModule.kt
+++ data/src/main/java/com/dev6/data/di/networkModule.kt
@@ -1,11 +1,36 @@
 package com.dev6.data.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-class networkModule {
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpRequestInterceptor())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("https://pokeapi.co/api/v2/")
+//            .addConverterFactory(MoshiConverterFactory.create())
+//            .addCallAdapterFactory(CoroutinesResponseCallAdapterFactory.create())
+            .build()
+    }
+
+
 
 }
---
+++ data/src/main/java/com/dev6/data/service/LoginAPI.kt
@@ -0,0 +1,14 @@
+package com.dev6.data.service
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface LoginAPI {
+
+    @GET("pokemon")
+    suspend fun fetchPokemonList(
+        @Query("limit") limit: Int = 20,
+        @Query("offset") offset: Int = 0
+    ): ApiResponse<PokemonResponse>
+
+}
