15a8130ef37248c9b9171e393200db7abcc60387
ChoiHyoSeok <36808015+supremehyo@users.noreply.github.com>
2022-06-07 19:51:20 +0900

    .

--- features/feed/src/main/java/com/dev6/feed/view/profileDetailFragment/ProfileDailyFragment.kt
+++ features/feed/src/main/java/com/dev6/feed/view/profileDetailFragment/ProfileDailyFragment.kt
@@ -14,7 +14,7 @@ class ProfileDailyFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+      
         return inflater.inflate(R.layout.fragment_profile_daily, container, false)
     }
 
