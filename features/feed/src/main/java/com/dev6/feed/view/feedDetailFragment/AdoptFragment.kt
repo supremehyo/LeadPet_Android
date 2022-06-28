9997e3d88f498dd47f2510de164c3c4a8ef393de
ChoiHyoSeok <36808015+supremehyo@users.noreply.github.com>
2022-06-29 00:38:02 +0900

    [add] 카테고리 , 정렬 bottomSheet

--- features/feed/src/main/java/com/dev6/feed/view/feedDetailFragment/AdoptFragment.kt
+++ features/feed/src/main/java/com/dev6/feed/view/feedDetailFragment/AdoptFragment.kt
@@ -7,17 +7,19 @@ import com.dev6.core.base.BindingFragment
 import com.dev6.feed.R
 import com.dev6.feed.adapter.AdoptAdapter
 import com.dev6.feed.databinding.FragmentAdoptBinding
+import com.dev6.feed.option.BottomSheet
 import com.dev6.feed.viewmodel.FeedViewModel
 
 
-class AdoptFragment : BindingFragment<FragmentAdoptBinding>(R.layout.fragment_adopt)  {
+class AdoptFragment : BindingFragment<FragmentAdoptBinding>(R.layout.fragment_adopt) {
 
-    private  val feedViewModel : FeedViewModel by activityViewModels()
+    private val feedViewModel: FeedViewModel by activityViewModels()
+    private lateinit var bottomSheet: BottomSheet
 
     override fun initView() {
         binding.adoptRc.apply {
             layoutManager = LinearLayoutManager(context)
-            adapter = AdoptAdapter{
+            adapter = AdoptAdapter {
 
             }
         }
@@ -28,7 +30,16 @@ class AdoptFragment : BindingFragment<FragmentAdoptBinding>(R.layout.fragment_ad
     }
 
     override fun initListener() {
-        binding.adoptSpinner
+        binding.adoptOption.setOnClickListener {
+            bottomSheet.show(childFragmentManager, bottomSheet.tag)
+            bottomSheet.show(childFragmentManager, bottomSheet.tag).also {
+                BottomSheet({
+                    when (it) {
+
+                    }
+                }, "정렬")
+            }
+        }
     }
 
 }
--- features/feed/src/main/java/com/dev6/feed/view/feedDetailFragment/DonationFragment.kt
+++ features/feed/src/main/java/com/dev6/feed/view/feedDetailFragment/DonationFragment.kt
@@ -1,30 +1,41 @@
 package com.dev6.feed.view.feedDetailFragment
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import com.dev6.core.base.BindingFragment
 import com.dev6.feed.R
 import com.dev6.feed.databinding.FragmentDonationBinding
+import com.dev6.feed.option.BottomSheet
 
 
 class DonationFragment : BindingFragment<FragmentDonationBinding>(R.layout.fragment_donation) {
+
+    private lateinit var bottomSheet: BottomSheet
 
     override fun initView() {
 
     }
 
-    override fun initViewModel() {
-
-    }
-
     override fun initListener() {
+        binding.apply {
+            donationOption1.setOnClickListener {
+                bottomSheet.show(childFragmentManager, bottomSheet.tag).also {
+                    BottomSheet({
+                        when (it) {
 
+                        }
+                    }, "정렬")
+                }
+            }
+            donationOption2.setOnClickListener {
+                bottomSheet.show(childFragmentManager, bottomSheet.tag).also {
+                    BottomSheet({
+                        when (it) {
+
+                        }
+                    }, "정")
+                }
+            }
+        }
     }
 
-    override fun afterViewCreated() {
-
-    }
 }
